d959198b8a950b2e5f048426bd40b54eb2b3ed48
Alexandre Abruzzese <alexandre.abruzzese@ynov.com>
2018-03-26 09:55:58 +0200

    Hello world

--- app.py
+++ app.py
@@ -4,6 +4,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+app.config.from_object('secret_config')
+
+@app.route('/accueil')
+def helloworld():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
